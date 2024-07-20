ed438ae736b10a9504a274843676946e30187280
abdelwahedyosri <abdelwahed_yosri@yahoo.com>
2024-07-20 14:42:59 +0100

    microservice-project-finalisation

---
+++ GatewayServere/src/main/java/timsoft/gateway/gatewayserverer/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package timsoft.gateway.gatewayserverer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+//@Configuration
+//@EnableWebFluxSecurity
+public class SecurityConfig {
+/*
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChainSecurityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers("/eureka/**")
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated())
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+        return serverHttpSecurity.build();
+    }
+
+*/
+}
